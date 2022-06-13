37fea1f1d347a273303b89a4dc942a2fc6561f9e
Yohan <john@inertiainmotion.com.au>
2022-06-13 21:24:33 +1000

    Bring back ModuleControls placeholder

---
+++ backend-editor/gui/src/ModuleControls.vue
@@ -0,0 +1,145 @@
+<template>
+  <div
+    class="module-controls flex text-center"
+    :data-testid="`${type || component.type}-controls`"
+  >
+    <ul
+      class="list-unstyled flex m-0 p-0 gap-2 overflow-x-auto pb-0"
+      :class="[`flex-${getDirection}`]"
+    >
+      <li
+        class="flex-shrink-0"
+        v-for="(setting, i) in settings"
+        :key="component.uuid + type + i"
+        :title="setting.title"
+      >
+        <component
+          v-if="setting.component"
+          :component="component"
+          :value="value"
+          :index="index"
+          :is="setting.component"
+        />
+        <eva-icon
+          v-else
+          :name="setting.icon"
+          @click="setting.action"
+          width="18"
+          height="18"
+          fill="currentColor"
+          class="flex cursor-pointer pulse"
+          :class="[setting.class || '']"
+        ></eva-icon>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+// import { recursifyID } from "../../utils/id";
+// import { UCFirst } from "../../functions/helpers";
+// import { createModule } from "../../factories/modules/moduleFactory";
+// import { EvaIcon } from "vue-eva-icons";
+// import ModuleSelector from "../ModuleSelector.vue";
+// import SettingStack from "../shared/SettingStack.vue";
+// import ClipboardFunctions from "../../mixins/ClipboardFunctions";
+
+export default {
+  props: {
+    value: Array,
+    index: {
+      type: Number,
+      default: 0,
+    },
+    customSettings: {
+      type: Object,
+      default: () => {},
+    },
+    direction: {
+      type: String,
+    },
+    component: {
+      type: Object,
+      required: true,
+    },
+    type: String,
+  },
+  // mixins: [ClipboardFunctions],
+  computed: {
+    settings(): object[] {
+      return Object.values({
+        menu: {
+          icon: "edit-2-outline",
+          title: "Open settings modal",
+          component: "setting-stack",
+          // action: this.openModal,
+          order: 10,
+        },
+        add: {
+          icon: "plus-circle",
+          title: "Add module right after this module",
+          // action: this.addModule,
+          order: 20,
+        },
+        clone: {
+          icon: "copy",
+          title: "Copy module",
+          // action: this.cloneModule,
+          // condition: this.component,
+          order: 30,
+        },
+        clipboardCopy: {
+          // icon: this.isValidPasteLocation ? "clipboard" : "clipboard-outline",
+          title: "Copy module to clipboard",
+          // action: this.isValidPasteLocation
+          //   ? this.pasteFromClipboard
+          //   : this.copyToClipboard,
+          order: 30,
+          // class: this.isValidPasteLocation ? "pulse-constant" : "",
+        },
+        delete: {
+          icon: "trash-2",
+          title: "Delete module",
+          // action: this.removeModule,
+          order: 40,
+        },
+        // ...this.customSettings,
+      })
+        .filter((el: any) => el)
+        .sort(
+          (a: { order: number }, b: { order: number }) => a.order - b.order
+        );
+    },
+    getDirection(): string {
+      return "";
+      // this.component.type === "field" ||
+      // this.component.type === "set"
+      // ? "row"
+      // : "col";
+    },
+  },
+  // components: {
+  //   EvaIcon,
+  //   ModuleSelector,
+  //   SettingStack,
+  // },
+  methods: {
+    addModule() {
+      // const newModule = createModule(UCFirst(this.type || this.component.type));
+      // this.value.splice(this.index + 1, 0, newModule);
+    },
+    cloneModule(newModule: any) {
+      // let clone = newModule || JSON.parse(JSON.stringify(this.component));
+      // Generate ID's for each nested module
+      // recursifyID(clone);
+      // this.value.splice(this.index + 1, 0, clone);
+    },
+    removeModule() {
+      // this.value.splice(this.index, 1);
+    },
+    openModal(name: string) {
+      // this.$modals.open(name);
+    },
+  },
+};
+</script>
