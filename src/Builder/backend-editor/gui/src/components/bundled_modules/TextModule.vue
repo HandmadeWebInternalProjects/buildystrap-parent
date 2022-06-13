8da637b482fe2c82a19a88aebbd6b54141262f17
Yohan <john@inertiainmotion.com.au>
2022-06-13 20:27:42 +1000

    Remove from src

--- src/Builder/backend-editor/gui/src/components/bundled_modules/TextModule.vue
+++
@@ -1,5 +0,0 @@
-<script lang="ts" setup></script>
-<template lang="">
-  <div class="bg-700 shadow-sm text-white text-center rounded-1 p-2">Test</div>
-</template>
-<style lang=""></style>
--- src/Builder/backend-editor/gui/src/components/fieldtypes/TextFieldtype.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-  <div>
-    <input placeholder="text fieldtype" type="text" />
-  </div>
-</template>
-
-<style lang=""></style>
