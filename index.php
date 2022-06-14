6ccff7f6a365495a6692a4a6284c6a6056c28b73
Michael Rook <michael@rook.net.au>
2022-06-14 14:04:32 +1000

    Render builder or yield content

--- index.php
+++ index.php
@@ -4,7 +4,5 @@ use Buildystrap\Theme;
 
 defined('ABSPATH') || exit;
 
-echo Buildystrap\Builder::renderFromContent(get_the_content())->render();
-
 // Blade
 echo view()->first(Theme::get_template_hierarchy(), get_defined_vars())->render();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,9 @@
 @include('partials.header')
 
-@yield('content')
+@if(json_decode(get_the_content()))
+    {!! Buildystrap\Builder::renderFromContent(get_the_content())->render() !!}
+@else
+    @yield('content')
+@endif
 
 @include('partials.footer')
