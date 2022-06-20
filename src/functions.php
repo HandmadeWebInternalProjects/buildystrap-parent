311bfbb68499d3faa3aa2cd0ef9a3057477a88da
Michael Rook <michael@rook.net.au>
2022-06-20 14:20:59 +1000

    Use new register method for script

--- src/functions.php
+++ src/functions.php
@@ -3,21 +3,11 @@
 use Buildystrap\Builder;
 use Buildystrap\BuilderBackend;
 use Buildystrap\Theme;
-use Buildystrap\Builder\Modules\Text;
 
 add_action('init', [Builder::class, 'boot']);
 add_action('acf/init', [BuilderBackend::class, 'boot']);
 add_action('init', [Theme::class, 'boot']);
 
-
- add_action('buildystrap::builder::boot', function () {
-        Builder::registerAddon(
-            'text',
-            Text::class,
-            __DIR__.'/resources/views',
-            [
-                'stylesheet' => '',
-                'script' => get_template_directory_uri().'/buildy-addons/test/test.js',
-            ]
-        );
-    });
+add_action('buildystrap::builder::boot', function () {
+    Builder::registerBackendScript('text', get_template_directory_uri().'/buildy-addons/test/test.js');
+});
