be3fa88758b1474d90cee215ace5f2a3e092ee7c
Yohan <john@handmadeweb.com.au>
2022-07-10 15:25:47 +1000

    Swap TitleField's card markup to the new bs-card

--- backend-editor/gui/src/components/fields/TitleField.vue
+++ backend-editor/gui/src/components/fields/TitleField.vue
@@ -14,64 +14,61 @@ watch(value.value, (newValue) => {
 </script>
 
 <template>
-  <div class="card border-0 bg-100">
-    <div class="card-header pb-1 border-0">
-      <field-label
-        v-if="config.label !== false"
-        :label="config?.label !== undefined ? config.label : handle" />
-    </div>
-    <field-group>
-      <div class="d-flex flex-column gap-3 card-body">
-        <richtext-field
-          class="w-100"
-          handle="text"
-          :config="{
-            ...config,
-            label: false,
-          }"
-          v-model="value['text']" />
-        <div class="d-flex gap-4">
-          <radio-buttons-field
-            class=""
-            handle="level"
-            :uuid="uuid"
+  <bs-card :label="config.label">
+    <template v-slot:body>
+      <field-group>
+        <div class="d-flex flex-column gap-3 card-body">
+          <richtext-field
+            class="w-100"
+            handle="text"
             :config="{
               ...config,
-              label: 'Level',
-              options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+              label: false,
             }"
-            v-model="value['level']" />
-          <select-field
-            class="flex-grow-1 flex-basis-0"
-            handle="size"
-            :config="{
-              label: 'Size',
-              options: ['h1', 'h2'],
-              placeholder: 'Default',
-              taggable: true,
-            }"
-            v-model="value['size']" />
-          <select-field
-            class="flex-grow-1 flex-basis-0"
-            handle="colour"
-            :config="{
-              label: 'Colour',
-              options: ['Primary', 'Secondary'],
-              placeholder: 'Default',
-              taggable: true,
-            }"
-            v-model="value['colour']" />
+            v-model="value['text']" />
+          <div class="d-flex gap-4">
+            <radio-buttons-field
+              class=""
+              handle="level"
+              :uuid="uuid"
+              :config="{
+                ...config,
+                label: 'Level',
+                options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+              }"
+              v-model="value['level']" />
+            <select-field
+              class="flex-grow-1 flex-basis-0"
+              handle="size"
+              :config="{
+                label: 'Size',
+                options: ['h1', 'h2'],
+                placeholder: 'Default',
+                taggable: true,
+              }"
+              v-model="value['size']" />
+            <select-field
+              class="flex-grow-1 flex-basis-0"
+              handle="colour"
+              :config="{
+                label: 'Colour',
+                options: ['Primary', 'Secondary'],
+                placeholder: 'Default',
+                taggable: true,
+              }"
+              v-model="value['colour']" />
+          </div>
+          <text-field
+            class=""
+            handle="class"
+            type="text"
+            :value="value['class']"
+            placeholder=""
+            :config="{ label: 'Class' }" />
         </div>
-        <text-field
-          class=""
-          handle="class"
-          type="text"
-          :value="value['class']"
-          placeholder=""
-          :config="{ label: 'Class' }" />
-      </div>
-    </field-group>
-  </div>
+      </field-group>
+    </template>
+  </bs-card>
 </template>
 
 <style lang="scss">
