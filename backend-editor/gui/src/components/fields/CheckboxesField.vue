25361c2ac4956fc59a5cdc0990ad11b9dc820ed6
Yohan <john@handmadeweb.com.au>
2022-10-04 17:50:31 +1100

    Checkbox / visibility tab fix

--- backend-editor/gui/src/components/fields/CheckboxesField.vue
+++ backend-editor/gui/src/components/fields/CheckboxesField.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { useFieldType, commonProps } from "./useFieldType"
 import { generateID } from "@/utils/id"
-import { computed, toRefs, onMounted } from "vue"
+import { ref, watch, toRefs, onMounted } from "vue"
 import * as bootstrap from "bootstrap"
 
 const props = defineProps({
@@ -20,15 +20,12 @@ const isReadOnly = config.value.readOnly || false
 const emit = defineEmits(["update:modelValue", "updateMeta"])
 const { update, normaliseOptions } = useFieldType(emit)
 
-const options : any = normaliseOptions(config.value.options) || []
+const options: any = normaliseOptions(config.value.options) || []
 
-const values = computed<any>({
-  get() {
-    return modelValue?.value || []
-  },
-  set(val) {
-    update(val)
-  },
+const selected = ref<string[]>(modelValue?.value ?? [])
+
+watch(selected, (value) => {
+  update(value)
 })
 
 onMounted(() => {
@@ -54,25 +51,29 @@ onMounted(() => {
     <div
       class="checkboxes-fieldtype-wrapper d-flex gap-1"
       :class="{ 'inline-mode': config?.inline }">
-      <div class="option" v-for="(option, $index) in options" :key="$index">
+      <div
+        class="option"
+        v-for="option in options"
+        :key="`${uuid}-${option.value}`">
         <input
           type="checkbox"
           class="btn-check"
           :id="`${handle}_${uuid}_[${option.value}]`"
-          :name="`${handle}[${option.value}]`"
           :value="option.value"
           :disabled="isReadOnly"
-          v-model="values" />
-        <label class="checkbox-label btn btn-sm btn-primary" :for="`${handle}_${uuid}_[${option.value}]`">
+          v-model="selected" />
+        <label
+          class="checkbox-label btn btn-sm btn-primary"
+          :for="`${handle}_${uuid}_[${option.value}]`">
           {{ option.label || option.value }}
-        </label>  
+        </label>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
-  .checkbox-label {
-    text-transform: none !important;
-  }
+.checkbox-label {
+  text-transform: none !important;
+}
 </style>
