86d4042717497625f44d33f70785eebee3055e35
Yohan <john@handmadeweb.com.au>
2022-10-18 12:19:09 +1100

    Add some logic to allow certain modules in rows (divider)
    Add divider module

--- backend-editor/gui/src/components/sections/GridSection.vue
+++ backend-editor/gui/src/components/sections/GridSection.vue
@@ -20,6 +20,13 @@ const section = ref(props.component || {})
 const parentArray = ref(props.parentArray || [])
 const sectionIndex = computed(() => props.sectionIndex || 0)
 const rows = ref(props.component?.rows)
+
+const handleDrop = (to: any, from: any, el: any) => {
+  const validTypes = ["row", "divider-module"]
+  const type = el._underlying_vm_?.type || false
+
+  return type && validTypes.includes(type)
+}
 </script>
 
 <template>
@@ -36,15 +43,24 @@ const rows = ref(props.component?.rows)
     <div class="flex-grow-1">
       <draggable
         :list="rows"
-        group="rows"
+        :group="{
+          name: 'rows',
+          put: handleDrop,
+        }"
         handle=".sortable-handle"
         item-key="uuid"
         class="section-draggable d-flex flex-grow-1 p-3 flex-grow flex-column gap-3 group">
         <template #item="{ element, index }">
           <grid-row
+            v-if="element.type === 'row'"
             :row-index="index"
             :parent-array="rows"
             :component="element" />
+          <module-base
+            v-else
+            :component="element"
+            :columns="rows"
+            :component-index="index" />
         </template>
       </draggable>
     </div>
---
+++ src/Builder/Modules/DividerModule.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Buildystrap\Builder\Modules;
+
+use Buildystrap\Builder\Extends\Module;
+
+class DividerModule extends Module
+{
+  protected static function blueprint(): array
+  {
+    return [
+      'icon' => 'fa-solid fa-paragraph',
+      'fields' => [
+        'height' => [
+          'config' => [
+            'label' => 'Height',
+          ],
+          'type' => 'text-field',
+        ],
+      ],
+    ];
+  }
+}
