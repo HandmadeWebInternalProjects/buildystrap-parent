cd30e639cd4ae3df999a6957d05e8b08ad66da83
Aaron Curle <aaron@handmadeweb.com.au>
2023-04-17 19:52:17 +1000

    Fix for videos without autoplay enabled

--- resources/js/components/videos.js
+++ resources/js/components/videos.js
@@ -1,7 +1,8 @@
 export default function () {
     const videoOverlays = document.querySelectorAll('.video-overlay');
     videoOverlays.forEach(function (videoOverlay) {
-        let playButton = videoOverlay.querySelector('.video-play');
+        let playButton = videoOverlay?.querySelector('.video-play');
+        if (!playButton) return;
         playButton.addEventListener('click', function (e) {
             e.preventDefault();
             let parent = e.target.closest('.buildystrap-video-module'),
