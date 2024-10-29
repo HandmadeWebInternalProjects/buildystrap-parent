b7a7240ef98f5601cb5d4fc42817d08829bf62ec
Yohan <john@handmadeweb.com.au>
2024-10-29 16:16:31 +1100

    Add filter to video play icon SVG for easy replacement

--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -34,10 +34,11 @@ $video_thumb = isset($video_thumb) && $video_thumb ? wp_get_attachment_image_url
         <div class="video-overlay__contents">
           <a href="javascript:;" class="video-play">
             <span class="visually-hidden">Play video</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="124" height="125" viewBox="0 0 124 125" fill="none">
+            {{-- You can add_filter in child theme to change the icon --}}
+            {!! apply_filters( 'video-play-icon', '<svg xmlns="http://www.w3.org/2000/svg" width="124" height="125" viewBox="0 0 124 125" fill="none">
               <path d="M123 62.9713C123 96.66 95.6894 123.97 62 123.97C28.3106 123.97 1 96.66 1 62.9713C1 29.2827 28.3106 1.97266 62 1.97266C95.6894 1.97266 123 29.2827 123 62.9713Z" stroke="currentColor" stroke-width="2"/>
               <path fill-rule="evenodd" clip-rule="evenodd" d="M49.459 87.7549L84.5591 67.2513C85.8333 66.5563 86.7601 65.0504 86.7601 63.4286C86.7601 61.8069 85.9492 60.301 84.6749 59.6059L49.459 39.1023C48.1847 38.4073 46.3312 38.4073 45.057 39.1023C43.7827 39.7974 42.856 41.3033 42.856 42.925V83.9322C42.856 85.554 43.7827 87.0599 45.057 87.7549C45.752 88.2183 46.5629 88.3341 47.258 88.3341C47.953 88.3341 48.7639 88.2183 49.459 87.7549Z" fill="currentColor"/>
-            </svg>
+            </svg>' ) !!}
           </a>
         </div>
       @endif
