f2f2d538748adc30ac0431fc9a92e1f5c6663ec3
Yohan <john@handmadeweb.com.au>
2024-11-01 14:01:18 +1100

    Fix card module....

--- resources/views/builder/modules/card-module.blade.php
+++ resources/views/builder/modules/card-module.blade.php
@@ -1,64 +1,47 @@
-@props([
-    'post' => null,
-    'media' => null,
-    'mediaLightbox' => null,
-    'type' => 'image',
-    'title',
-    'titleLevel' => 'h4',
-    'permalink' => null,
-    'content' => null,
-    'class' => '',
-])
-
-{{-- 
-The following slots are available:
-- beforeTitle
-- afterTitle 
-- tags
---}}
-
 @php
-  $wrapper_tag = $permalink ? 'a' : 'div';
+  $direction = $module->get('image_placement') ?? 'flex-column';
+  $card_classes = collect(['card', $direction])->filter()->implode(' ');
+  $button_group_class = $module->get('button_group_class') ?? null;
+  $button_group_alignment = $module->get('button_group_alignment') ?? null;
+  $title_spacing = $module->get('title_spacing')?->value() ?? null;
+
+  $alignment = get_responsive_classes(module: $module, prop: 'button_group_alignment', fallback: '', classPrefix: 'align-self');
+
+  if ($button_group_alignment) {
+    $button_group_class .= " $alignment";
+  }
 @endphp
 
-<{{ $wrapper_tag }} @if($permalink) href="{{ $permalink }}" @endif class="card media-card {{ $class }}">
-  <div class="media-card__inner">
-    @isset($media)
-      <div class="media-card__media media-card__media-{{ $type }} position-relative">
-        @if ($mediaLightbox)
-          @component('components.cards.media.' . $type, ['media' => $media, 'mediaLightbox' => $mediaLightbox])
-          @endcomponent
-        @else
-          @component('components.cards.media.' . $type, ['media' => $media])
-          @endcomponent
+@extends('builder::module-base', ['class' => $card_classes])
+
+@section('field_content')
+    <div class="card-image">
+        @if($module->has('image'))
+          @php
+          $aspect_ratio = $module->get('image_aspect_ratio') ? "aspect-ratio: {$module->get('image_aspect_ratio')};" : '';
+          @endphp
+          {!! wp_get_attachment_image(collect($module->get('image')->value())->first()['id'], 'medium', '', ['class' => 'card-img object-cover h-auto', 'style' => $aspect_ratio]) !!}
+        @endif
+    </div>
+
+    <div class="card-body" @isset($title_spacing) style="--bs-card-title-spacer-y: {{ $title_spacing }};" @endisset>
+      @if($module->has('title'))
+        {{ $module->get('title')->withClass('card-title') }}
+      @endif
+      
+      <div class="card-content">
+        @if($module->has('body'))
+          {!! $module->get('body') !!}
         @endif
       </div>
-    @endisset
-    <div class="card-body media-card__content">
-      @isset($beforeTitle)
-        {!! $beforeTitle !!}
-      @endisset
-      <div class="d-flex">
-        <{{ $titleLevel }} class="media-card__title">
-          {!! $title ?? '' !!}
-        </{{ $titleLevel }}>
-      </div>
-      @isset($afterTitle)
-        {!! $afterTitle !!}
-      @endisset
-      @if(isset($content) && !empty($content))
-        <div class="media-card__content">
-          {!! $content !!}
+      
+      @if($module->has('button_groups'))
+        <div class="button-group {{ $button_group_class }}">
+          @foreach($module->get('button_groups')->value() as $button_group)
+            @component('builder.components.button', ['button' => $button_group['button']]) @endcomponent
+          @endforeach
         </div>
-      @endisset
-      @isset($tags)
-        <div class="media-card__tags">
-          {!! $tags !!}
-        </div>
-      @endisset
-      @isset($bodyEnd)
-        {!! $bodyEnd !!}
-      @endisset
-    </div>
+      @endif
   </div>
-</{{ $wrapper_tag }}>
+
+@overwrite
