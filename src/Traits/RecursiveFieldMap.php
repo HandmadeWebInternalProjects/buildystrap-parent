35f37b87bdfc9c2aed237b042a7890db62cfcfb0
Yohan <john@handmadeweb.com.au>
2024-11-01 16:46:24 +1100

    Make RecursiveFieldMap actually recursive

--- src/Traits/RecursiveFieldMap.php
+++ src/Traits/RecursiveFieldMap.php
@@ -13,45 +13,48 @@ trait RecursiveFieldMap
     $blueprintFields = static::getBlueprint()->get('fields');
 
     $this->fields = $this->collectionClass($module['values'])->map(function ($value, $handle) use ($blueprintFields) {
-      if (!empty($blueprintFields[$handle]) && $blueprintField = $blueprintFields[$handle]) {
-        if ($field = Builder::getField($blueprintField['type'])) {
-          $fieldValue = $value;
+      return $this->processField($value, $handle, $blueprintFields);
+    })->filter();
+  }
 
-          // Replicators
-          if (is_array($fieldValue) && isset($blueprintField['fields'])) {
+  private function processField($value, $handle, $blueprintFields)
+  {
+    if (!empty($blueprintFields[$handle]) && $blueprintField = $blueprintFields[$handle]) {
+      if ($field = Builder::getField($blueprintField['type'])) {
+        $fieldValue = $value;
 
-            $fieldValue = collect($fieldValue)->map(function ($subField) use ($blueprintFields, $handle) {
-              if (is_array($subField)) {
-                foreach ($subField as $subHandle => $subValue) {
-                  if (
-                    isset($blueprintFields[$handle]['fields'][$subHandle]['type']) &&
-                    $subFieldType = Builder::getField($blueprintFields[$handle]['fields'][$subHandle]['type'])
-                  ) {
-                    $subField[$subHandle] = new $subFieldType($subValue);
-                    
-                  }
+        // Replicators
+        if (is_array($fieldValue) && isset($blueprintField['fields']) && $blueprintField['type'] === 'replicator-field') {
+          $fieldValue = collect($fieldValue)->map(function ($subField) use ($blueprintFields, $handle) {
+            if (is_array($subField)) {
+              foreach ($subField as $subHandle => $subValue) {
+                if (
+                  isset($blueprintFields[$handle]['fields'][$subHandle]['type']) &&
+                  $subFieldType = Builder::getField($blueprintFields[$handle]['fields'][$subHandle]['type'])
+                ) {
+                  $subField[$subHandle] = $this->processField($subValue, $subHandle, $blueprintFields[$handle]['fields']);
                 }
               }
-              return $subField;
-            });
-
-            return new $field($fieldValue);
-          }
-
-          // Groups
-          if (isset($blueprintField['fields'])) {
-            $fieldValue = collect($fieldValue)->map(function ($subField, $handle) use ($blueprintField) {
-              if (($blueprintField['fields'][$handle]['type'] ?? null) && $subFieldType = Builder::getField($blueprintField['fields'][$handle]['type'])) {
-                return new $subFieldType($subField);
-              }
-            });
-          }
+            }
+            return $subField;
+          });
 
           return new $field($fieldValue);
         }
-      }
 
-      return null;
-    })->filter();
+        // Groups
+        if (isset($blueprintField['fields']) && $blueprintField['type'] === 'group-field') {
+          $fieldValue = collect($fieldValue)->map(function ($subField, $subHandle) use ($blueprintField) {
+            if (($blueprintField['fields'][$subHandle]['type'] ?? null) && $subFieldType = Builder::getField($blueprintField['fields'][$subHandle]['type'])) {
+              return $this->processField($subField, $subHandle, $blueprintField['fields']);
+            }
+          });
+        }
+
+        return new $field($fieldValue);
+      }
+    }
+
+    return null;
   }
 }
