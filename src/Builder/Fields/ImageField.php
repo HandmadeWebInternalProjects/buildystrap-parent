0324cfe475efe166ebdcfde0eeb681eb3b247b08
Yohan <john@handmadeweb.com.au>
2024-12-12 13:57:41 +1100

    Update toHtml of image field

--- src/Builder/Fields/ImageField.php
+++ src/Builder/Fields/ImageField.php
@@ -9,32 +9,55 @@ use function wp_get_attachment_image;
 
 class ImageField extends Field
 {
-    protected static function blueprint(): array
-    {
-        return [
-          'config' => [],
-        ];
+  protected static function blueprint(): array
+  {
+    return [
+      'config' => [],
+    ];
+  }
+
+  public function __toString(): string
+  {
+    $image = isset($this->value()['image']) ? $this->value()['image'] : null;
+    if (! $image) {
+      return false;
+    }
+    return collect($image)
+      ->map(fn($item) => wp_get_attachment_image_url($item['id'], 'full'))
+      ->first();
+  }
+
+  public function toHtml(): string
+  {
+    global $bodhi_svgs_options;
+    $image = optional(collect($this->value()['image'] ?? []))->pluck('id')->first();
+    $inline_svg = optional($bodhi_svgs_options)['force_inline_svg'] === 'on';
+    $width = optional($this->value())['width'] ? "width: {$this->value()['width']};" : '';
+    $max_width = optional($this->value())['max_width'] ? "max-width: {$this->value()['max_width']};" : '';
+    $height = optional($this->value())['height'] ? "height: {$this->value()['height']};" : '';
+    $max_height = optional($this->value())['max_height'] ? "max-height: {$this->value()['max_height']};" : '';
+
+    $classes = collect([
+      optional($this->value())['object_fit'] ? "object-{$this->value()['object_fit']}" : 'full',
+      optional($this->value())['object_position'] ? "object-position-{$this->value()['object_position']}" : null,
+    ])->filter()->implode(' ');
+
+    if (! $image) {
+      return false;
     }
 
-    public function __toString(): string
-    {
-        $image = isset($this->value()['image']) ? $this->value()['image'] : null;
-        if ( ! $image) {
-            return false;
-        }
-        return collect($image)
-          ->map(fn ($item) => wp_get_attachment_image_url($item['id'], 'full'))
-          ->first();
+    $html = '';
+    if ($inline_svg) {
+      $html .= "<div class='svg-container' style='" . trim("$width $max_width $height $max_height") . "'>";
     }
-
-    public function toHtml(): string
-    {
-        $image = isset($this->value()['image']) ? $this->value()['image'] : null;
-        if ( ! $image) {
-            return false;
-        }
-        return collect($image)
-          ->map(fn ($item) => wp_get_attachment_image($item['id'], 'full'))
-          ->implode('');
+    $html .= wp_get_attachment_image($image, 'full', false, [
+      'class' => $classes,
+      'alt' => $this->value()['alt'] ?? '',
+      'style' => trim("$width $max_width $height $max_height"),
+    ]);
+    if ($inline_svg) {
+      $html .= '</div>';
     }
+    return $html;
+  }
 }
