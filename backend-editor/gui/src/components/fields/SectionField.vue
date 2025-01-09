70980a55e92a9d866158b164a08d344d7562d8e1
Yohan <john@handmadeweb.com.au>
2025-01-09 21:30:51 +1100

    Add ability for custom settings in sectionField

--- backend-editor/gui/src/components/fields/SectionField.vue
+++ backend-editor/gui/src/components/fields/SectionField.vue
@@ -16,17 +16,26 @@ import { useFieldType, commonProps } from "./useFieldType"
 const props = defineProps({ ...commonProps })
 import { createModule } from "../../factories/modules/moduleFactory"
 
-const customSettings = {
+const { handle, config, modelValue } = toRefs(props)
+const emit = defineEmits(["update:modelValue", "updateMeta"])
+const { update } = useFieldType(emit)
+const section = createModule("Section")
+
+const customSettings: { [key: string]: boolean } = {
   menu: false,
   add: false,
   clone: false,
   delete: false,
 }
 
-const { handle, config, modelValue } = toRefs(props)
-const emit = defineEmits(["update:modelValue", "updateMeta"])
-const { update } = useFieldType(emit)
-const section = createModule("Section")
+if (config?.value.customSettings) {
+  // If custom setings are passed in with true values, remove them from the default settings
+  Object.keys(config.value.customSettings).forEach((key) => {
+    if (config.value.customSettings[key]) {
+      delete customSettings[key]
+    }
+  })
+}
 
 onBeforeMount(() => {
   if (modelValue && !modelValue?.value) {
